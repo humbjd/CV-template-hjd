b6e407f68e57adc0d7f1037f66c4b83b98ae1603
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2024-02-13 17:47:14 -0300

    Ajustes de codigo

--- scripts/main.js
+++ scripts/main.js
@@ -5,28 +5,3 @@ AOS.init({
 });
 
 // Add your javascript here
-const puppeteer = require('puppeteer');
-
-document.getElementById('btnDownload').addEventListener('click', function() {
-  // Chamar a função salvarPaginaComoPDF quando o botão for clicado
-  salvarPaginaComoPDF('https://humbertojd-cv.vercel.app/', 'Humberto_Dantas.pdf');
-});
-
-async function salvarPaginaComoPDF(url, nomeArquivo) {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-
-  // Navegar para a página desejada
-  await page.goto(url, { waitUntil: 'networkidle2' });
-
-  // Esperar um curto período para garantir que todos os recursos estejam carregados
-  await page.waitForTimeout(2000);
-
-  // Gerar o arquivo PDF
-  await page.pdf({ path: nomeArquivo, format: 'A4' });
-
-  // Fechar o navegador
-  await browser.close();
-
-  console.log(`PDF salvo como ${nomeArquivo}`);
-}
